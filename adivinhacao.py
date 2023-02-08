e29723dd2b75b7b1c4096afca4a92f6b2dde26f1
Bia <biameloni@gmail.com>
2023-02-07 22:28:34 -0300

    Diga de chute, se o número chutado é maior ou menor

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,16 +2,22 @@ print("---------------------------------")
 print("Bem vindo ao jogo de adivinhação!")
 print("---------------------------------")
 
-numero_secreto = 42
+numero_secreto: int = 42
 
 chute_str = input("Digite o seu número: ")
 print("Você digitou ", chute_str)
 
 chute = int(chute_str)
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
 
-if (numero_secreto == chute):
+if (acertou):
     print("Parabéns, você acertou o número!")
 else:
-    print("Você errou o número!")
+    if(maior):
+        print("O seu chute foi maior que o número secreto")
+    elif(menor):
+        print("O seu chute foi menor que o número secreto")
 
 print("\nFim de jogo")
