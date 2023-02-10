cfb0650a73e6ec363b216c4d7bf754c5521db5a7
Bia <biameloni@gmail.com>
2023-02-09 22:30:29 -0300

    Trocando o while pelo for

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,7 +6,7 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range(1,total_de_tentativas + 1):
     print("\nTentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu número: ")
     print("\nVocê digitou ", chute_str)
@@ -16,14 +16,12 @@ while(rodada <= total_de_tentativas):
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
 
-    if (acertou):
+    if(acertou):
         print("\nParabéns, você acertou o número!")
     else:
         if(maior):
             print("O seu chute foi maior que o número secreto")
         elif(menor):
             print("O seu chute foi menor que o número secreto")
-
-    rodada = rodada + 1
 
 print("Fim de jogo")
